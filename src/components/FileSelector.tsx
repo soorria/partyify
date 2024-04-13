d9d93ef4f4a44a30cdeb6f60e6ff92f5c9eca8ed
Soorria Saruva <soorria.ss@gmail.com>
2024-04-13 15:26:03 +1000

    feat: add ability to drop an image on the page

--- src/components/FileSelector.tsx
+++ src/components/FileSelector.tsx
@@ -1,24 +1,31 @@
-import React, { useRef } from 'react';
+import React, { ChangeEvent } from 'react';
+import useDrop from 'react-use/esm/useDrop';
 
 export default function FileSelector(props: { onFileSelected: (fileUrl: File | undefined) => void }): JSX.Element {
-	const fileInputRef = useRef<HTMLInputElement>(null);
+	const state = useDrop({
+		onFiles(files) {
+			if (files.length) {
+				props.onFileSelected(files[0]);
+			}
+		},
+	});
 
-	const handleFileChange = () => {
-		props.onFileSelected(fileInputRef.current?.files?.[0]);
+	const handleFileChange = (e: ChangeEvent<HTMLInputElement>) => {
+		props.onFileSelected(e.currentTarget.files?.[0]);
 	};
 
 	return (
 		<>
-			<input
-				type='file'
-				id='file-input'
-				className='FileSelector__Input'
-				onChange={handleFileChange}
-				ref={fileInputRef}
-			/>
+			<input type='file' id='file-input' className='FileSelector__Input' onChange={handleFileChange} />
 			<label htmlFor='file-input' className='Button FileSelector__Label'>
-				Pick an image...
+				Drop an image or pick one...
 			</label>
+
+			{state.over && (
+				<div className='FileSelector__DropIndicator'>
+					<div className='Button'>Drop it to party-ify!</div>
+				</div>
+			)}
 		</>
 	);
 }
